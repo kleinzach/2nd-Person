04a66e95c4446527395dcca54fbcdc8043ffb951
kleinzach <henry.eastman@gmail.com>
2015-01-24 08:49:09 -0500

    Prefabs and menus

---
+++ 2nd-Person/Assets/IngameMenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IngameMenuManager : MonoBehaviour {
+
+	public Canvas menu;
+
+	// Use this for initialization
+	void Start () {
+		menu.gameObject.SetActive(false);
+	}
+	
+	public void toggleMenu(){
+		menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			toggleMenu();
+		}
+	}
+}
