b0f8e18505fcc43644c9f3a90fed3676ede2af1d
abagraba <abagraba@gmail.com>
2015-01-24 08:53:55 -0500

    :octocat: :octopus:

--- 2nd-Person/Assets/Waypoint.cs
+++ 2nd-Person/Assets/Waypoint.cs
@@ -19,4 +19,16 @@ public class Waypoint : Usable{
 		if (target != null)
 			target.use ();
 	}
+
+	public void OnDrawGizmosSelected(){
+		Gizmos.color = Color.cyan;
+		Transform t = transform.parent;
+		if (t != null){
+			Path p = t.gameObject.GetComponent<Path>();
+			if (p != null)
+				foreach (Waypoint w in p.waypoints())
+					if (w != this)
+						Gizmos.DrawSphere(w.transform.position, 0.5f);
+		}
+	}
 }
