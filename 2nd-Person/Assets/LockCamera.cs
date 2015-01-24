a38421575f5cf0a93a8a68e467f93e74cc758397
abagraba <abagraba@gmail.com>
2015-01-24 04:07:43 -0500

    :yum: :baby:

--- 2nd-Person/Assets/LockCamera.cs
+++ 2nd-Person/Assets/LockCamera.cs
@@ -26,6 +26,42 @@ public class LockCamera : MonoBehaviour {
 		delta.Normalize ();
 		transform.position = delta * distance + target.transform.position;
 		transform.rotation = Quaternion.LookRotation (-delta);
-		Debug.DrawLine (transform.position, transform.position + -delta * distance, Color.red);
+		drawCamera(1);
+	}
+
+	public void drawCamera(float z){
+		Camera camera = gameObject.camera;
+		
+		Vector3 s1 = camera.ScreenToWorldPoint(new Vector3(0, 0, 1));
+		Vector3 s2 = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, 1));
+		Vector3 s3 = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, 1));
+		Vector3 s4 = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, 1));
+
+		Vector3 r1 = transform.position - forceToZ(s1 - transform.position, z);
+		Vector3 r2 = transform.position - forceToZ(s2 - transform.position, z);
+		Vector3 r3 = transform.position - forceToZ(s3 - transform.position, z);
+		Vector3 r4 = transform.position - forceToZ(s4 - transform.position, z);
+
+		Color c = Color.red;
+	
+		Debug.DrawLine (r1, r2, c);
+		Debug.DrawLine (r2, r4, c);
+		Debug.DrawLine (r4, r3, c);
+		Debug.DrawLine (r3, r1, c);
+		Debug.DrawLine (r1, r4, c);
+		Debug.DrawLine (r3, r2, c);
+		Debug.DrawLine (transform.position, r2, c);
+		Debug.DrawLine (transform.position, r4, c);
+		Debug.DrawLine (transform.position, r3, c);
+		Debug.DrawLine (transform.position, r1, c);
+	}
+
+	/**
+	 * Takes a direction vector and forces it to the z plane. The vector returned is such that (transform.position - the vector) results in a point on the x-y plane with z coordinate equal to z.
+	 * v.z should not be 0.
+	 **/
+	private Vector3 forceToZ(Vector3 v, float z){
+		v *= (transform.position.z - z) / Mathf.Abs(v.z);
+		return v;
 	}
 }
