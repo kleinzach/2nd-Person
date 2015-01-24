95a6c593d69921830773bddec4e56d16da62c317
kleinzach <henry.eastman@gmail.com>
2015-01-24 01:17:54 -0500

    Working on Waypoints/Triggers

---
+++ 2nd-Person/Assets/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : Usable {
+
+	public Waypoint next;
+
+	public float speed;
+
+	public bool open;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public override void use(){
+		open = true;
+	}
+}
