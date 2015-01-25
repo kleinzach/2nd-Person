4f8b348c52e8fd7a2725aa5637d4634b5b88d77a
abagraba <abagraba@gmail.com>
2015-01-25 13:49:07 -0500

    :laughing: :muscle:

--- 2nd-Person/Assets/Scripts/Background.cs
+++ 2nd-Person/Assets/Scripts/Background.cs
@@ -6,12 +6,14 @@ public class Background : MonoBehaviour {
 	public float scrollSpeed;
 	public float x;
 	private float lastX;
+	public string sortinglayer = "Default";
 
 	void Start () {
 		x = Random.value;
 	}
 
 	void Update () {
+		renderer.sortingLayerName = sortinglayer;
 		float dx = x - transform.position.x * scrollSpeed;
 		if (lastX != dx){
 			GetComponent<MeshFilter>().mesh.uv = new Vector2[]{new Vector2(1 + dx, 0), new Vector2(dx, 1), new Vector2(dx, 0), new Vector2(1 + dx, 1)};
