bc53000c697a59d5501a1b7de52cbdff9ce41cfd
kleinzach <henry.eastman@gmail.com>
2015-01-25 14:22:15 -0500

    Particle fixes

---
+++ 2nd-Person/Assets/Prefabs/SceneLayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.sortingLayerName = "Scene";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
